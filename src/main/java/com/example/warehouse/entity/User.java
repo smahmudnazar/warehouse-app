0c10c45fc1ffeb35bc343edef0c16e21fa917669
muzaffarmirzakarimov <mmirzakarimov05@gmail.com>
2022-03-08 12:00:35 +0500

    start

---
+++ src/main/java/com/example/warehouse/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.warehouse.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+}
